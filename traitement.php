142a6c1eefb3decdac3267fd79bf6c470301dc0f
Hugo Espenan <espenanhugo@gmail.com>
2023-01-13 16:18:43 +0100

    j'ai encore modifié

--- traitement.php
+++ traitement.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     $_SESSION['nom'] = $_POST['name'];
-    $_SESSION['prenom'] = $_POST['forename'];
+    $_SESSION['prenom'] = $_POST['forname'];
     $_SESSION['birth'] = $_POST['birth'];
     $_SESSION['email'] = $_POST['email'];
 
